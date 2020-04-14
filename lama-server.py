dfa37a012d2dda6c480ff647893076d5a30868b2
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-04-14 18:08:05 +0530

    Modify Server to run TestMaster instead of client

--- lama-server.py
+++ lama-server.py
@@ -1,10 +1,8 @@
-from game import GameMaster
-from twisted.internet import reactor, endpoints
-from twisted.web import server
+from game import TestMaster
 import sys
 
 if __name__ == "__main__":
-    g = GameMaster()
-    endpoint = endpoints.TCP4ServerEndpoint(reactor, 1144)
-    endpoint.listen(server.Site(g))
-    reactor.run()
+ 
+    with TestMaster() as test:
+    	test.init()
+    	test.run()
