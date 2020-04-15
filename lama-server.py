3a677edf66d91941a3aac80d22c847bad28043ef
Akash Cherukuri <62202731+LightDrifter@users.noreply.github.com>
2020-04-15 18:42:01 +0530

    Revert lama-server.py to original

--- lama-server.py
+++ lama-server.py
@@ -1,8 +1,10 @@
-from game import TestMaster
+from game import GameMaster
+from twisted.internet import reactor, endpoints
+from twisted.web import server
 import sys
 
 if __name__ == "__main__":
- 
-    with TestMaster() as test:
-    	test.init()
-    	test.run()
+    g = GameMaster()
+    endpoint = endpoints.TCP4ServerEndpoint(reactor, 1144)
+    endpoint.listen(server.Site(g))
+    reactor.run()
